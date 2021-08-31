39fa472c17c98f273b90355be6a49a2da53fd974
palitexBot <rafonseca4+@gmail.com>
2021-08-31 22:33:27 +0000

    Everson1.0.1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const Discord
+const {Client,MessageEmbed,Intents} = require("discord.js")
+const client = new Client({intents:["DIRECT_MESSAGES","GUILDS","GUILD_MEMBERS","GUILD_MESSAGES","GUILD_BANS","GUILD_PRESENCES"]})
+const config = require('./config/infos')
+client.login(config.token)
+client.on("ready",a=>{
+console.log(`Logado no ${a.user.tag}!`)
+    
+})
